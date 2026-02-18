0d24593c4248945ce10ec9406129069ac3a8fd47
Arnav299 <asachdeva230@gmail.com>
2026-02-18 12:28:40 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
-  basePath: "/clone",        // 👈 repo name
-  assetPrefix: "/clone/",
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
